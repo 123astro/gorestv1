6a46fd550c138ec19e31ba46bbbaabfcf9b0ff55
Keith Benjamin <kjb528i@yahoo.com>
2022-03-21 17:17:25 -0400

    added delete

--- src/main/java/com/example/gorestv1/contollers/UserController.java
+++ src/main/java/com/example/gorestv1/contollers/UserController.java
@@ -3,33 +3,64 @@ package com.example.gorestv1.contollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping ("/api/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
     Environment env;
 
     // url / endpoint http://localhost:4444/api/user/token
-    @GetMapping ("/token")
+    @GetMapping("/token")
     public String getToken() {
         return env.getProperty("GOREST_TOKEN");
     }
 
+    //URL / endpoint GET http://localhost:4444/api/user/{id}
     @GetMapping("/{id}")
-    public Object getOneUser(@PathVariable ("id") String userId,
-        RestTemplate restTemplate){
+    public Object getOneUser(@PathVariable("id") String userId,
+                             RestTemplate restTemplate
+    ) {
         try {
             String url = "https://gorest.co.in/public/v2/users/" + userId;
             return restTemplate.getForObject(url, Object.class);
-        } catch (Exception exception){
+        } catch (Exception exception) {
             return "404: No user exist with the ID " + userId;
         }
     }
+
+    //URL / endpoint DELETE http://localhost:4444/api/user/{id}
+    @DeleteMapping("/{id}")
+    public Object deleteOneUser(@PathVariable("id") String userId,
+                                RestTemplate restTemplate) {
+        try {
+
+            String url = "https://gorest.co.in/public/v2/users/" + userId;
+            String token = env.getProperty("GOREST_TOKEN");
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(token);
+            //headers.set("Authorization", Bearer " + token);
+
+            HttpEntity request = new HttpEntity(headers);
+            restTemplate.exchange(
+                    url,
+                    HttpMethod.DELETE,
+                    request,
+                    Object.class
+            );
+            //  restTemplate.delete(url);
+            return "Successfully Deleted user# " + userId;
+        } catch (Exception exception) {
+            return exception.getMessage();
+        }
+    }
+
+
 }
